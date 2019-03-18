dc5670fdf557f86b191d4bf6f90bfc2e40cab596
TIAGO RIBEIRO SARAIVA DE ANDRADE <p000tandrade@prservicos.com.br>
2019-03-18 11:31:06 -0300

    Maven Project

---
+++ habse/src/main/java/crud/habse/App.java
@@ -0,0 +1,13 @@
+package crud.habse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
